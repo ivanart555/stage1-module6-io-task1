1ac0f1aee325cd4532ae7419ffa736728004f5f6
Ivan Artsiushyn <ivanart555@gmail.com>
2023-07-17 22:08:34 +0300

    Solve task

---
+++ src/main/java/com/epam/mjc/io/ContentParser.java
@@ -0,0 +1,23 @@
+package com.epam.mjc.io;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class ContentParser {
+
+    public Map<String, String> parseContent(List<String> content) {
+        Map<String, String> parsed = new HashMap<>();
+
+        for (String str:content) {
+            String[] keyValue = str.split(": ");
+            parsed.put(keyValue[0].toLowerCase(Locale.ROOT), keyValue[1]);
+       }
+
+        return parsed;
+
+    }
+
+
+}
--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,40 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        List<String> content = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new java.io.FileReader(file))){
+            String line = br.readLine();
+
+            while(line != null) {
+                content.add(line);
+                line = br.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        } catch (IOException e) {
+            System.out.println("Something went wrong with file operations.");
+        }
+
+        ContentParser contentParser = new ContentParser();
+        Map<String, String > contentMap = contentParser.parseContent(content);
+
+        Profile profile = new Profile();
+        profile.setName(contentMap.get("name"));
+        profile.setAge(Integer.parseInt(contentMap.get("age")));
+        profile.setEmail(contentMap.get("email"));
+        profile.setPhone(Long.parseLong(contentMap.get("phone")));
+
+        return profile;
     }
 }
